5d5e4c8b88e08ed948f3ab35160502dae635daa1
paras <vi@exilent.systems>
2019-06-24 14:31:37 +0530

    Added new endpoints for getting android and ios relative builds based on version and club Removed unused command from ahoy Updated docker compose to have a volume mapping from Builds directory to build directory in resources

--- src/main/java/ffa/exilent/systems/buildserver/controller/IndexController.java
+++ src/main/java/ffa/exilent/systems/buildserver/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
@@ -15,9 +16,9 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/ios/manifest")
-    public ResponseEntity<Resource> downloadManifest() {
-        Resource resource = new ClassPathResource("builds/ios/manifest.plist");
+    @GetMapping("/ios/{version}/{club}/manifest")
+    public ResponseEntity<Resource> downloadManifest(@PathVariable("version") String version,@PathVariable("club") String club) {
+        Resource resource = new ClassPathResource("builds/"+version+"/"+club+"/ios/manifest.plist");
 
         // Try to determine file's content type
         String contentType = null;
@@ -33,9 +34,9 @@ public class IndexController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
-    @GetMapping("/ios/ipa")
-    public ResponseEntity<Resource> downloadIPA() {
-        Resource resource = new ClassPathResource("builds/ios/syd.ipa");
+    @GetMapping("/ios/{version}/{club}/ipa")
+    public ResponseEntity<Resource> downloadIPA(@PathVariable("version") String version,@PathVariable("club") String club) {
+        Resource resource = new ClassPathResource("builds/"+version+"/"+club+"/ios/"+club+".ipa");
 
         // Try to determine file's content type
         String contentType = null;
@@ -51,9 +52,9 @@ public class IndexController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
                 .body(resource);
     }
-    @GetMapping("/android/apk")
-    public ResponseEntity<Resource> downloadApk() {
-        Resource resource = new ClassPathResource("builds/android/adl.apk");
+    @GetMapping("/android/{version}/{club}/apk")
+    public ResponseEntity<Resource> downloadApk(@PathVariable("version") String version,@PathVariable("club") String club) {
+        Resource resource = new ClassPathResource("builds/android/"+version+"/"+club+"/"+club+"");
 
         // Try to determine file's content type
         String contentType = null;
